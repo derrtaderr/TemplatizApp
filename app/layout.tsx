a1ed32a853fe572355504da259ce332a68d115a5
Jason Derr <dharrmabum@gmail.com>
2025-02-04 21:43:04 -0800

    Set default theme to dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
             attribute="class"
             enableSystem
             disableTransitionOnChange
-            defaultTheme="light"
+            defaultTheme="dark"
           >
             {children}
           </ThemeProvider>
