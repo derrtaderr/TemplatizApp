38b06f52f5f7fa6c8c49f20040b68f1dcb9ffd69
Jason Derr <dharrmabum@gmail.com>
2025-02-17 15:45:04 -0800

    Update GitHub link in footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -42,7 +42,7 @@ export const Footer = () => {
     },
     {
       name: "GitHub",
-      href: "https://github.com/derrtaderr",
+      href: "#",
     },
   ];
   return (
@@ -53,7 +53,7 @@ export const Footer = () => {
             <div className="mr-4  md:flex mb-4">
               <Logo />
             </div>
-            <div>Copyright &copy; 2025 Templatiz</div>
+            <div>Copyright &copy; 2025 Templatiz LLC.</div>
             <div className="mt-2">All rights reserved</div>
           </div>
           <div className="grid grid-cols-3 gap-10 items-start mt-10 md:mt-0">
